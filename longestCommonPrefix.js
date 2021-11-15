f6dc77d84727430f95586cb1ec74dbebf8ab1ddd
Rana Osama Awadallah <rana.osama.awadallah@gmail.com>
2021-11-15 21:55:34 +0100

    Longest Common Prefix Solution
    Test cases:
    ["flower","flow","flight"]
    ["dog","racecar","car"]
    ["dog"]
    ["fuck", "fun", "bla"]
    ["cir","car"]
    ["flower","fkow"]

---
+++ longestCommonPrefix.js
@@ -0,0 +1,46 @@
+/** Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters. */
+
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 1) return strs[0];
+    let commonString = "";
+    
+    for(let i = 0; i < strs.length - 1; i++) {
+        let curr = strs[i];
+        let nxt = strs[i+1];
+        let minLen = Math.min(curr.length, nxt.length);
+        let common = '';
+        
+        for(let j = 0; j < minLen; j++) {
+            if(curr[j] === nxt[j]) common += nxt[j];
+            else j = minLen;
+        }
+        
+        if(!common) return '';
+        
+        if(common.length < commonString.length || !commonString) commonString = common;
+    
+    }
+
+    return commonString;
+};
