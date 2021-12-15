ae5b41b6155cdaeab73e323cddc0cc67b9f3ecbf
Rana Osama Awadallah <rana.osama.awadallah@gmail.com>
2021-12-15 22:08:56 +0100

    169. Majority Element
    https://leetcode.com/problems/majority-element/

---
+++ majorityElement.js
@@ -0,0 +1,36 @@
+/**
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-231 <= nums[i] <= 231 - 1
+**/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const map = {};
+    
+    for(const num of nums) {
+        map[num] = (map[num] || 0) + 1;
+        if (map[num] > nums.length / 2) return num;
+    }
+};
